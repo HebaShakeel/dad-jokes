5e854aefa02f732992b20c351667943ddac3da4a
Heba Shakeel <hebashakeel295@gmail.com>
2020-12-28 11:39:34 +0530

    dad-jokes

---
+++ script.js
@@ -0,0 +1,33 @@
+const  jokeEl = document.getElementById('joke')
+const jokeBtn = document.getElementById('jokeBtn')
+
+jokeBtn.addEventListener('click',generateJoke)
+
+generateJoke()
+
+async function generateJoke(){
+    const config = {
+        headers: {
+            'Accept' : 'application/json'
+        }
+    }
+
+    const response = await fetch("https://icanhazdadjoke.com",config) 
+
+    const data = await response.json()
+
+    jokeEl.innerHTML = data.joke
+    
+}
+
+/*function generateJoke(){
+    const config = {
+        headers: {
+            'Accept' : 'application/json'
+        }
+    }
+
+    fetch("https://icanhazdadjoke.com",config).then((response) => response.json()).then((data) => {
+        jokeEl.innerHTML = data.joke
+    })
+}*/
